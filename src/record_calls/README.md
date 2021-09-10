b020cdefb16b617d5c3da4501543493eef1ce785
Hoel Bagard <hoel.bagard@gmail.com>
2021-09-10 11:25:37 +0900

    Added the exception handling part to the record calls exercise

--- src/record_calls/README.md
+++ src/record_calls/README.md
@@ -1,10 +1,32 @@
-Sources on decorators:
+### Sources on decorators:
 https://realpython.com/primer-on-python-decorators/
 https://www.pythonmorsels.com/exercises/3ee85ad3481f428d99458b102cbda7c6/submit/1/
 
 
+### Notes:
 
-Useful snipets / templates:
+About preserving the doctring and function info:\
+This doesn't quite pass our tests though. Unfortunately, the record_calls class will still have an unhelpful string representation and it still won't have quite the right documentation.
+We could try to fix this ourselves, but it's kind of a pain. The easiest way to fix this is to rely on the third-party wrapt library.
+
+```
+import wrapt
+class record_calls(wrapt.ObjectProxy):
+    """Record calls to the given function."""
+    def __init__(self, func):
+        super().__init__(func)
+        self.call_count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.call_count += 1
+        return self.__wrapped__(*args, **kwargs)
+```
+The wrapt library has a lot of decorator helpers in it, but the one that we're concerned with is the one that allows us to make a class which will act as a seamless wrapper around a function. It's a little bit awkward to use wrapt.ObjectProxy, but it works.
+
+I don't usually show third-party libraries in these solutions and I didn't expect you to discover this one, but I wanted you to know it exists because it can come in handy at times (for making any kind of decorator, not just ones that use classes).
+
+
+### Useful snipets / templates:
 
 Basic boiler plate template:
 ```
--- src/record_calls/record_calls.py
+++ src/record_calls/record_calls.py
@@ -1,14 +1,18 @@
 import functools
+import traceback
 from dataclasses import dataclass
+from typing import Optional
+
+
+NO_RETURN = object()
 
 
 @dataclass
 class Call:
     args: any
     kwargs: any
-
-
-NO_RETURN = object()
+    return_value: any = NO_RETURN
+    exception: Optional[Exception] = None
 
 
 class record_calls:
@@ -20,8 +24,13 @@ class record_calls:
 
     def __call__(self, *args, **kwargs):
         self.call_count += 1
-        self.calls.append(Call(args, kwargs))
-        return self.func(*args, **kwargs)
+        try:
+            return_value = self.func(*args, **kwargs)
+            self.calls.append(Call(args, kwargs, return_value, None))
+            return return_value
+        except Exception as e:
+            self.calls.append(Call(args, kwargs, NO_RETURN, e))
+            raise
 
 
 def main():
@@ -34,20 +43,40 @@ def main():
         print(f"Hello {name}")
 
     # Basic Exercise
+    print("\nBasic exercise")
     greet("Trey")
     print(f"{greet.call_count=}")
     greet(name="Trey")
     print(f"{greet.call_count=}")
 
     # Bonus 1
+    print("\nBonus 1")
     help(greet)
+    print(str(greet))
 
     # Bonus 2
+    print("\nBonus 2")
     print(f"{greet.calls[0].args=}")
     print(f"{greet.calls[0].kwargs=}")
 
     print(f"{greet.calls[1].args=}")
     print(f"{greet.calls[1].kwargs=}")
+
+    # Bonus 3
+    print("\nBonus 3")
+
+    @record_calls
+    def cube(n):
+        return n**3
+
+    print(f"{cube(3)=}")
+    print(cube.calls)
+    try:
+        cube(None)
+    except Exception as e:
+        print(traceback.format_exc())
+    print(cube.calls[-1].exception)
+    print(cube.calls[-1].return_value == NO_RETURN)
 
 
 if __name__ == "__main__":
