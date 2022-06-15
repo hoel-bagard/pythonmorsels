e518267e546dfaf83205ff883ab97a7322540185
Hoel Bagard <hoel.bagard@gmail.com>
2022-06-15 10:26:36 +0900

    Began working on deep_flatten

---
+++ src/deep_flatten/README.md
@@ -0,0 +1,5 @@
+# [Deep Flatten](https://www.pythonmorsels.com/exercises/05fe60de17824ef3ae77d93a3be2e98b)
+
+
+Run the python morsels tests with `python src/deep_flatten/test_deep_flatten.py`.\
+Run the main file with `python src/deep_flatten/deep_flatten.py`.
---
+++ src/deep_flatten/deep_flatten.py
@@ -0,0 +1,47 @@
+from typing import Iterable, TypeVar
+T = TypeVar('T')
+
+
+def deep_flatten_g(my_input) -> Iterable[T]:
+    for item in my_input:
+        if isinstance(item, Iterable):
+            yield from deep_flatten(item)
+        else:
+            yield item
+
+
+def deep_flatten(iterable):
+    return list(deep_flatten_g(iterable))
+
+
+def test_equal(res, expected_res):
+    assert res == expected_res, f"Wrong result, got:\n\t{res}\nbut expected\n\t {expected_res}"
+
+
+def main():
+    # Base exercise:
+    test_equal(deep_flatten([[(1, 2), (3, 4)], [(5, 6), (7, 8)]]), [1, 2, 3, 4, 5, 6, 7, 8])
+    test_equal(deep_flatten([[1, [2, 3]], 4, 5]), [1, 2, 3, 4, 5])
+
+    # Bonus 1
+    # Make sure your deep_flatten function accepts not just lists and tuples, but generators, sets,
+    # and other iterable data structures (but don't worry about handling strings yet, as we'll handle them in bonus 3).
+    test_equal(sorted(deep_flatten({(1, 2), (3, 4), (5, 6), (7, 8)})), [1, 2, 3, 4, 5, 6, 7, 8])
+
+    # Bonus 2
+    # For the second bonus, I'd like you to make deep_flatten return an iterator that loops over input lazily.
+
+    numbers_and_words = enumerate([99, 98, 97])
+    flattened = deep_flatten(numbers_and_words)
+    test_equal(next(flattened), 0)
+    test_equal(next(flattened), 99)
+    test_equal(next(numbers_and_words), (1, 98))
+
+    # Bonus 3
+    # For the third bonus, I'd like you to make sure deep_flatten works with strings:
+    test_equal(list(deep_flatten([['apple', 'pickle'], ['pear', 'avocado']])), ['apple', 'pickle', 'pear', 'avocado'])
+    print("Passed the tests.")
+
+
+if __name__ == "__main__":
+    main()
