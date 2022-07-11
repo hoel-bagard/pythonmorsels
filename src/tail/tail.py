00ae99c013ffeb751f465b65b05c173cfc03ce18
Hoël Bagard <34478245+hoel-bagard@users.noreply.github.com>
2022-07-11 10:16:03 +0900

    Hoel/tail (#9)
    * Added tail readme.
    
    * Added tail's tests
    
    * Added tail excercise
    
    * Fixed typing.
    
    * Removed the for loop.
    
    * Added note to readme.
    
    * Pep8 fix

---
+++ src/tail/tail.py
@@ -0,0 +1,40 @@
+from collections import deque
+from collections.abc import Iterable
+from typing import TypeVar
+
+
+T = TypeVar('T')
+
+
+def tail(iterable: Iterable[T], nb_elts: int) -> list[T]:
+    if nb_elts <= 0:
+        return []
+
+    return list(deque(iterable, maxlen=nb_elts))
+
+
+def test_equal(res: list[T], expected_res: list[T]) -> None:
+    assert res == expected_res, f"Wrong result, got:\n\t{res}\nbut expected\n\t {expected_res}"
+
+
+def main():
+    # Base exercise:
+    print("Testing the base exercise")
+    test_equal(tail([1, 2, 3, 4, 5], 3), [3, 4, 5])
+    test_equal(tail("hello", 2), ['l', 'o'])
+    test_equal(tail("hello", 0), [])
+
+    # Bonus 1
+    print("Testing bonus 1")
+    test_equal(tail('hello', -2), [])
+
+    # Bonus 2
+    print("Testing bonus 2")
+    squares = (n**2 for n in range(10))
+    test_equal(tail(squares, 3), [49, 64, 81])
+
+    print("Passed the tests!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/tail/test_tail.py
@@ -0,0 +1,47 @@
+"""Tests for the tail exercise using Pytest."""
+from collections.abc import Iterable
+from typing import TypeVar
+
+import pytest
+
+from src.tail.tail import tail
+
+
+T = TypeVar('T')
+
+
+@pytest.mark.parametrize("iterable, length, expected_value",
+                         [([1, 2], 0, []),
+                          ([1, 2], 1, [2]),
+                          ([1, 2], 2, [1, 2]),
+                          ("test", 3, ['e', 's', 't']),
+                          ((3, 2, 1), 3, [3, 2, 1])])
+def test_basic(iterable: Iterable[T], length: int, expected_value: list[T]):
+    assert tail(iterable, length) == expected_value
+
+
+@pytest.mark.parametrize("iterable, length, expected_value",
+                         [([1, 2, 3, 4], 10, [1, 2, 3, 4]),
+                          ([], 10, [])])
+def test_n_larger_than_iterable_length(iterable: Iterable[T], length: int, expected_value: list[T]):
+    assert tail(iterable, length) == expected_value
+
+
+@pytest.mark.bonus1
+class TestBonus1:
+    @pytest.mark.parametrize("iterable, length, expected_value",
+                             [([1, 2, 3, 4], -1, []),
+                              ((), -10, [])])
+    def test_negative_n(self, iterable: Iterable[T], length: int, expected_value: list[T]):
+        assert tail(iterable, length) == expected_value
+
+
+@pytest.mark.bonus2
+class TestBonus2:
+    def test_iterator(self):
+        nums = (n**2 for n in [1, 2, 3, 4])
+        assert tail(nums, -1) == []  # Don't loop for negative n
+        assert tail(nums, 0) == []  # Don't loop for n=0
+        assert tail(nums, 2) == [9, 16]  # Consuming the generator
+        assert list(nums) == []  # The nums generator is now empty
+        assert tail(nums, 1) == []  # n=1 with a now empty generator
