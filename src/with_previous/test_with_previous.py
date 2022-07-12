295cd8e7a8d2fbd722b882aec3b73260ae28a5a3
Hoël Bagard <34478245+hoel-bagard@users.noreply.github.com>
2022-07-12 12:20:35 +0900

    Hoel/with previous (#10)
    * Added exercise answer.
    
    * Added readme with instructions.
    
    * Added tests.
    
    * Added notes to the readme.
    
    * Fixed typing.

---
+++ src/with_previous/test_with_previous.py
@@ -0,0 +1,60 @@
+"""Tests for the with_previous exercise using Pytest."""
+from collections.abc import Iterable
+from typing import Optional, TypeVar
+
+import pytest
+
+from src.with_previous.with_previous import with_previous
+
+
+T = TypeVar('T')
+
+
+def assert_equal_iterable(iterable1: Iterable[Optional[T]], iterable2: Iterable[Optional[T]]) -> None:
+    assert list(iterable1) == list(iterable2)
+
+
+@pytest.mark.parametrize("iterable, expected_value",
+                         [([1, 2, 3], [(1, None), (2, 1), (3, 2)]),
+                          (['1', '2'], [('1', None), ('2', '1')]),
+                          ([None, None], [(None, None), (None, None)])
+                          ])
+def test_basic(iterable: Iterable[Optional[T]], expected_value: list[Optional[T]]):
+    assert_equal_iterable(with_previous(iterable), expected_value)
+
+
+def test_empty():
+    assert_equal_iterable(with_previous([]), [])
+
+
+def test_one_item():
+    assert_equal_iterable(with_previous([1]), [(1, None)])
+
+
+@pytest.mark.bonus1
+class TestBonus1:
+    def test_lazy_iterable(self):
+        generator = (n**2 for n in [1, 2, 3])
+        expected_outputs = [(1, None), (4, 1), (9, 4)]
+        assert_equal_iterable(with_previous(generator), expected_outputs)
+
+
+@pytest.mark.bonus2
+class TestBonus2:
+    def test_returns_lazy_iterable(self):
+        inputs = (n**2 for n in [1, 2, 3])
+        iterable = with_previous(inputs)
+        assert iterable == iter(iterable)
+        assert next(iterable) == (1, None)
+        assert list(inputs) != []
+
+
+@pytest.mark.bonus3
+class TestBonus3:
+    def test_fillvalue_as_keyword_argument_only(self):
+        """Test can be called with fillvalue (but only as keyword arg)."""
+        inputs = [1, 2, 3]
+        expected_outputs = [(1, 0), (2, 1), (3, 2)]
+        assert_equal_iterable(with_previous(inputs, fillvalue=0), expected_outputs)
+        with pytest.raises(TypeError):
+            with_previous(inputs, 0)  # type: ignore
---
+++ src/with_previous/with_previous.py
@@ -0,0 +1,46 @@
+from collections.abc import Iterable, Iterator
+from typing import Optional, TypeVar
+
+
+T = TypeVar('T')
+
+
+def with_previous(iterable: Iterable[T], *, fillvalue: Optional[T] = None) -> Iterator[tuple[T, T | None]]:
+    previous = fillvalue
+    for item in iterable:
+        yield (item, previous)
+        previous = item
+
+
+def assert_equal(res: list[tuple[T, Optional[T]]], expected_res: list[tuple[T, Optional[T]]]) -> None:
+    assert res == expected_res, f"Wrong result, got:\n\t{res}\nbut expected\n\t {expected_res}"
+
+
+def main():
+    # Base exercise:
+    print("Testing the base exercise")
+    assert_equal(list(with_previous("hello")), [('h', None), ('e', 'h'), ('l', 'e'), ('l', 'l'), ('o', 'l')])
+    assert_equal(list(with_previous([1, 2, 3])), [(1, None), (2, 1), (3, 2)])
+
+    # Bonus 1
+    print("Testing bonus 1")
+    assert_equal(list(with_previous(n**2 for n in [1, 2, 3])), [(1, None), (4, 1), (9, 4)])
+
+    # Bonus 2
+    print("Testing bonus 2")
+    assert_equal([next(with_previous([1, 2, 3]))], [(1, None)])
+
+    # Bonus 3
+    print("Testing bonus 3")
+    assert_equal(list(with_previous([1, 2, 3], fillvalue=0)), [(1, 0), (2, 1), (3, 2)])
+    try:
+        with_previous([1, 2, 3], 0)  # type: ignore
+        raise Exception("Should have raised a TypeError but did not")
+    except TypeError:
+        pass
+
+    print("Passed the tests!")
+
+
+if __name__ == "__main__":
+    main()
