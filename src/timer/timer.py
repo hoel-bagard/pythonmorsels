c060ed587874760f18cebc0e3bf11b2940deee28
Hoel Bagard <hoel.bagard@gmail.com>
2022-07-31 13:39:57 +0900

    Added Timer's first bonus.

--- src/timer/timer.py
+++ src/timer/timer.py
@@ -1,22 +1,13 @@
 import time
 from types import TracebackType
-from typing import Type, TypeVar
-# from dataclasses import dataclass
-
-T = TypeVar('T')
-
-
-# @dataclass
-# class Call:
-#     args: Any
-#     kwargs: Any
-#     exception: Optional[Exception] = None
+from typing import Type
 
 
 class Timer:
     def __init__(self):
         self.start = 0
         self.elapsed = 0
+        self.runs: list[float] = []
 
     def __enter__(self):
         self.start = time.perf_counter()
@@ -24,6 +15,7 @@ class Timer:
 
     def __exit__(self, exception_type: Type[Exception], exception: Exception, traceback: TracebackType) -> bool:
         self.elapsed = time.perf_counter() - self.start
+        self.runs.append(self.elapsed)
         return True
 
 
@@ -33,10 +25,22 @@ def assert_equal(res, expected_res) -> None:
 
 def main():
     # Base exercise:
-    print("Testing the base exercise")
+    print("Testing the base exercise.")
     with Timer() as timer:
         time.sleep(0.5)
     assert 0.5 < timer.elapsed < 0.6, f"Time was not measured properly, expecter 0.5 but got {timer.elapsed=}"
+
+    # Bonus 1
+    print("Testing the first bonus.")
+
+    timer = Timer()
+    with timer:
+        sum(range(2**24))
+    t1 = timer.elapsed
+    with timer:
+        sum(range(2**25))
+    t2 = timer.elapsed
+    assert_equal(timer.runs, [t1, t2])
 
     print("Passed the tests!")
 
