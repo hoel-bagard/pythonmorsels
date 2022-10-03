d593a642da40a98098420e5ed28fe2292e1a22b9
Hoel Bagard <hoel.bagard@gmail.com>
2022-10-03 11:13:58 +0900

    Fixed typing in FuzzyString.

--- src/fuzzy_string/fuzzy_string_v2.py
+++ src/fuzzy_string/fuzzy_string_v2.py
@@ -6,37 +6,37 @@ from functools import total_ordering
 from typing import TypeVar
 
 T = TypeVar("T")
-TFuzzyString = TypeVar("TFuzzyString", bound="FuzzyString")
+# TFuzzyString = TypeVar("TFuzzyString", bound="FuzzyString")
 
 
 @total_ordering
 class Ordered(ABC):
     """Mixin class which defines <=, >, >= based on < and ==."""
     @abstractmethod
-    def __lt__(self, other: str | TFuzzyString) -> bool:
+    def __lt__(self, other: str | "FuzzyString") -> bool:
         """Child class must implement __lt__."""
 
     @abstractmethod
-    def __eq__(self, other: str | TFuzzyString) -> bool:
+    def __eq__(self, other: str | "FuzzyString") -> bool:
         """Child class must implement __eq__."""
 
 
 class FuzzyString(Ordered, UserString):
     @staticmethod
-    def _preprocess_str(string: str | TFuzzyString) -> str:
+    def _preprocess_str(string: str | "FuzzyString") -> str:
         return unicodedata.normalize("NFKD", str(string.casefold()))
 
-    def __lt__(self, other: str | TFuzzyString) -> bool:
+    def __lt__(self, other: str | "FuzzyString" | object) -> bool:
         if isinstance(other, (str, FuzzyString)):
             return self._preprocess_str(self.data) < self._preprocess_str(str(other))
         return NotImplemented
 
-    def __eq__(self, other: str | TFuzzyString) -> bool:
+    def __eq__(self, other: str | "FuzzyString" | object) -> bool:
         if isinstance(other, (str, FuzzyString)):
             return self._preprocess_str(self.data) == self._preprocess_str(str(other))
         return NotImplemented
 
-    def __contains__(self, other: str | TFuzzyString) -> bool:
+    def __contains__(self, other: str | "FuzzyString" | object) -> bool:
         if isinstance(other, (str, FuzzyString)):
             return self._preprocess_str(other) in self._preprocess_str(self.data)
         return NotImplemented
